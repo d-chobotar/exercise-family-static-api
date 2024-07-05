7d778aa7d93de9817eb1e19c72c46db383abcecd
d-chobotar <162404570+d-chobotar@users.noreply.github.com>
2024-07-05 20:33:49 +0000

    jackson family api solution

--- src/app.py
+++ src/app.py
@@ -27,16 +27,26 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = jackson_family.get_all_members()
     return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_member_by_id(id):
+    response_body = jackson_family.get_member(id)
+    status_code = 200 if 'error' not in response_body else 404
+    return jsonify(response_body), status_code
+
+@app.route('/member', methods=['POST'])
+def handle_post_member():
+    member = request.get_json()
+    new_member_response = jackson_family.add_member(member)
+    return jsonify(new_member_response), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def handle_delete_member(id):
+    delete_member_response = jackson_family.delete_member(id)
+    return jsonify(delete_member_response), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,25 +11,47 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "id": self._generateId(),
+                "name": "John Jackson",
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "name": "Jane Jackson",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "name": "Jimmy Jackson",
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if 'id' not in member:
+            member['id'] = self._generateId()
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members[:] = [member for member in self._members if member['id'] != id]
+        return {"done": True}
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        member = next((member for member in self._members if member['id'] == id), None)
+        if member: 
+            return member
+        return {"error": f"member with id: {id} not found"}
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
